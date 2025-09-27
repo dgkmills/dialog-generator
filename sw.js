7bc1b8790b5f5da26bd4acbf47d61dbca43d84d4
Daniel G Mills <dgkmills@gmail.com>
2025-09-28 05:58:30 +0700

    Initial commit of dialog generator

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Define a name for the cache
+const CACHE_NAME = 'dialog-generator-cache-v2';
+// List the files to be cached for offline use
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/QRDGTool.jpeg' // Caching your QR code image
+];
+
+// Install event: fires when the service worker is first installed
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache and caching files');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: fires for every network request made by the page
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // If the request is in the cache, return the cached version
+        if (response) {
+          return response;
+        }
+        // Otherwise, fetch the request from the network
+        return fetch(event.request);
+      })
+  );
+});
+
